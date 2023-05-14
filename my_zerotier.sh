ed43ed703f5aebd07e29e4f42651d30e78a07b15
KacprkLipka <87283828+KacprkLipka@users.noreply.github.com>
2023-05-14 20:05:33 +0200

    Create my_zerotier.sh

---
+++ my_zerotier.sh
@@ -0,0 +1,41 @@
+#!bin/sh
+# my-zerotier
+
+ZEROTIER=zerotier_id
+API_TOKEN=api_token
+API_TOKEN=$(cat $API_TOKEN)
+HOST=TrueNAS
+TOKEN=/var/lib/zerotier-one/authtoken.secret
+
+Install_script_secure() {
+    curl -sL 'https://raw.githubusercontent.com/zerotier \
+    /ZeroTierOne/master/doc/contact%40zerotier.com.gpg' | $E gpg -v --import
+    if z=$(curl -sL 'https://install.zerotier.com/' | gpg); then echo "$z" | $E bash -v; fi
+}
+
+Get_MyId() { # Set MEMBER
+    MEMBER=$(zerotier-cli info)
+    MEMBER=${MEMBER#200 info }
+    MEMBER=${MEMBER%% *}
+}
+set -xe
+
+# install
+Install_script_secure
+zerotier-cli status
+zerotier-cli listnetworks
+
+# config
+chmod go+r $TOKEN # Używanie bez sudo
+zerotier-cli join $ZEROTIER
+Get_MyId
+
+curl -v POST "https://my.zerotier.com/api/network/${ZEROTIER}/member/${MEMBER}" \
+    -H "Content-Type: application/json" \
+    -H "Authorization: bearer ${API_TOKEN}" \
+    -d '{"name": "'$HOST'", "config": {"authorized": true} }'
+
+zerotier-cli status
+zerotier-cli listnetworks
+
+exit
