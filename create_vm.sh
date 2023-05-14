ff2ae1edf8a0c1d1bd10d65a62a99b3f2c7da475
KacprkLipka <87283828+KacprkLipka@users.noreply.github.com>
2023-05-14 20:04:09 +0200

    Create create_vm.sh

---
+++ create_vm.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+alias vbm="VBoxManage"
+vm_name="TrueNAS"
+os_name="Other_64"
+iso_path="/home/kacper/Desktop/Projekt/TrueNAS-SCALE-22.12.2.iso"
+medium_path="/home/kacper/Desktop/Projekt/VirtualBox"
+storage="10000"
+memory="10000"
+bridge="wlp3s0"
+
+vbm createvm --name $vm_name --ostype $os_name --register
+
+vbm storagectl $vm_name --name "SATA Controller" --add sata \
+    --controller IntelAHCI
+
+for i in 1 2 3; do
+    vbm createhd --filename $medium_path$vm_name'/'$vm_name'_'$i.vdi \
+        --size $storage --format VDI
+
+    vbm storageattach $vm_name --storagectl "SATA Controller" --port $i \ 
+    --device 0 --type hdd --medium $medium_path$vm_name'/'$vm_name'_'$i.vdi
+done
+
+vbm storagectl $vm_name --name "IDE Controller" --add ide
+
+vbm storageattach $vm_name --storagectl "IDE Controller" --port 0 --device 0 \
+    --type dvddrive --medium $iso_path
+
+vbm modifyvm $vm_name --memory $memory --vram 128
+vbm modifyvm $vm_name --nic1 bridged --bridgeadapter1 $bridge
+
+vbm startvm $vm_name
