1566a799a67653f5febb452d121712887ea8dd7a
KacprkLipka <87283828+KacprkLipka@users.noreply.github.com>
2023-05-25 01:07:54 +0200

    Update my_zerotier.sh

--- my_zerotier.sh
+++ my_zerotier.sh
@@ -9,8 +9,8 @@ TOKEN=/var/lib/zerotier-one/authtoken.secret
 
 Install_script_secure() {
     curl -sL 'https://raw.githubusercontent.com/zerotier \
-    /ZeroTierOne/master/doc/contact%40zerotier.com.gpg' | $E gpg -v --import
-    if z=$(curl -sL 'https://install.zerotier.com/' | gpg); then echo "$z" | $E bash -v; fi
+    /ZeroTierOne/master/doc/contact%40zerotier.com.gpg' | gpg -v --import
+    if z=$(curl -sL 'https://install.zerotier.com/' | gpg); then echo "$z" | bash -v; fi
 }
 
 Get_MyId() { # Set MEMBER
@@ -30,9 +30,9 @@ chmod go+r $TOKEN # Używanie bez sudo
 zerotier-cli join $ZEROTIER
 Get_MyId
 
-curl -v POST "https://my.zerotier.com/api/network/${ZEROTIER}/member/${MEMBER}" \
+curl -v POST "https://my.zerotier.com/api/network/$ZEROTIER/member/$MEMBER" \
     -H "Content-Type: application/json" \
-    -H "Authorization: bearer ${API_TOKEN}" \
+    -H "Authorization: bearer $API_TOKEN" \
     -d '{"name": "'$HOST'", "config": {"authorized": true} }'
 
 zerotier-cli status
